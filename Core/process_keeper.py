6a16eb407e1a441a9a991aa6120e866564600028
aweffr <aweffr@qq.com>
2017-03-23 14:01:03 +0800

    创建kepper组件。具体启动和终止进程。

---
+++ Core/process_keeper.py
@@ -0,0 +1,29 @@
+import psutil
+from process_watcher import getPidsByName
+from subprocess import PIPE
+
+GLOBAL_DEBUG = 0
+
+
+def process_killer(targetProcessName):
+    idList = getPidsByName(targetProcessName)
+    if GLOBAL_DEBUG:
+        print("process_killer( %s)" % targetProcessName, end=", ")
+        print("idList: %s" % str(idList))
+    for pid in idList:
+        try:
+            p = psutil.Process(pid)
+            p.kill()
+        except Exception as e:
+            print("Warning", e)
+
+
+def process_starter(path):
+    p = psutil.Popen([path, ], stdout=PIPE)
+    return p
+
+
+if __name__ == "__main__":
+    pass
+    # process_killer(target_process="qqbrowser.exe")
+    # process_starter("C:\Program Files (x86)\Tencent\QQBrowser\QQBrowser.exe")
