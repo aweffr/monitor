0a75a27ae9be7fccb71b9240164fd85b68dd9151
aweffr <aweffr@qq.com>
2017-05-08 10:11:55 +0800

    增加chkconfig配置

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# monitor_run        Startup script for the python_monitor
+# chkconfig: 2345 80 90
+# description: Run the monitor for CPU, process and memory.
+# processname: python
+
 dir=/usr/local/huami/python2.7
 nohup ${dir}/env/bin/python ${dir}/app.py > out_log 2>&1 &
